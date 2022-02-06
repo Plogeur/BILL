c3f1d8f15773a38ac774cf3996d94885cb26ed01
Plogeur <79613259+Plogeur@users.noreply.github.com>
2022-02-06 17:15:50 +0100

    Update Pipeline.bash

--- Pipeline.bash
+++ Pipeline.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 list_P="P1.2 P15.1 P15.5 P15.6 P15.8 P15.10 P33.1 P33.2 P33.6"
+repertoire_Ines='/students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE'
 
 function maFonction()
 
@@ -11,13 +12,13 @@ function maFonction()
   echo "------------------ variant n°$3 : $1 ------------------"
   echo "------------------------------------------------ "
   echo "------------------ seqkit seq : $1 ------------------"
-  srun -c 8 seqkit seq /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/$1.fastq -m $4 -o /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/Pconc$4$1.fastq
+  srun -c 8 seqkit seq $repertoire_Ines/$2/$1/$1.fastq -m $4 -o $repertoire_Ines/$2/$1/Pconc$4$1.fastq
   echo "------------------ mapping : $1 ------------------"
-  srun -c 8 minimap2 --MD -ax map-ont -t 6 /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/seq_ref/reference.fasta /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/Pconc$4$1.fastq -o /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.sam
+  srun -c 8 minimap2 --MD -ax map-ont -t 6 $repertoire_Ines/seq_ref/reference.fasta /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/Pconc$4$1.fastq -o /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.sam
   echo "------------------ samtools view1 : $1 ------------------"
-  srun -c 8 samtools view -ubS -@ 4 /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.sam -o /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.bam
+  srun -c 8 samtools view -ubS -@ 4 $repertoire_Ines/$2/$1/mapping$4$1.sam -o /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.bam
   echo "Conversion réussie du fichier mapping$4$1 du .sam en .bam"
-  echo "------------------ samtools sort : $1 ------------------"
+  echo "------------------ samtools : $1 ------------------"
   srun -c 8 samtools sort -l 0 -@ 4 -o /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.sorted.bam /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$2/$1/mapping$4$1.bam
   echo "Trie réussie du fichier mapping$4$1.bam"
   echo "------------------ samtools view2 : $1 ------------------"
@@ -56,7 +57,7 @@ function seqkit_stats2() { #avec PconcALL
   echo "------------------ seqkit stats ------------------"
   srun -c 8 seqkit stats /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/PconcAll.fastq -o $1/results_seqkit_all.txt | csvtk csv2md -t
   cat $1/results_seqkit_all
-  echo "Les résultats du seqkit de l'ensemble des variants est disponible dans le répertoire : $1/results_seqkit_all"
+  echo "Les résultats du seqkit de l'ensemble des variants sont aussi disponible dans le répertoire : $1/results_seqkit_all"
   echo "Fin du programme"
   exit
 }
@@ -79,7 +80,6 @@ read_entier='y' #par défaut on lance tout
 number_lenght='500' #par défaut taille 500 pour seqkit
 read_P='y' #par défaut on lance le P
 bool_seqkit='n'
-repertoire_seqkit='/students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/seqkit/'
 
 read -p "Réaliser une analyse avec seqkit pour déterminer la taille de fragment minimun ? (y/n)" bool_seqkit
 
