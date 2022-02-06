e0ed7a943e8e4187134704aed5b3ef28d0d53677
Plogeur <79613259+Plogeur@users.noreply.github.com>
2022-02-06 18:26:20 +0100

    Update Pipeline.bash

--- Pipeline.bash
+++ Pipeline.bash
@@ -33,6 +33,7 @@ function maFonction()
   echo "------------------ sniffles : $1 ------------------ "
   srun -c 8 sniffles -l 0 -m $repertoire_Ines/$2/$1/mapping$4$1.sorted.mapped.bam -t 4 -v $repertoire_Ines/$2/$1/mapping$4$1.sorted.mapped.vcf
   head $repertoire_Ines/$2/$1/mapping$4$1.sorted.mapped.vcf
+  #echo "------------------ IGV : $1 ------------------ "
   #commun/igv.sh
   duration=$SECONDS
   
@@ -46,9 +47,9 @@ function seqkit_stats() { #sans rien donc faire le cat des fastq + le cat des ca
   do
     if [ ! -d "Pconc$1$2.fastq" ]; then #concatene les fastq s'il n'existe pas
     {
-      cat /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$X/^FAQ|fastq_runid_\S*.fastq > /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/Pconc$1$2.fastq
+      cat $repertoire_Ines/$X/^FAQ|fastq_runid_\S*.fastq >  $repertoire_Ines/Pconc$1$2.fastq
     } else {
-      cat /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/$X/^FAQ|fastq_runid_\Pconc$1$2.fastq > /students/BILL/ines.boussiere/TP_2022/BRIVET_BOUSSIERE_BENARD_BAGARRE/Pconc$1$2.fastq
+      cat  $repertoire_Ines/$X/^FAQ|fastq_runid_\Pconc$1$2.fastq >  $repertoire_Ines/Pconc$1$2.fastq
     fi
     old_element=$element
 }
@@ -56,8 +57,8 @@ function seqkit_stats() { #sans rien donc faire le cat des fastq + le cat des ca
 function seqkit_stats2() { #avec PconcALL
   echo "------------------ seqkit stats ------------------"
   srun -c 8 seqkit stats $repertoire_Ines/PconcAll.fastq -o $repertoire_Ines/seqkit/results_seqkit_all.txt | csvtk csv2md -t
-  cat $1/results_seqkit_all
-  echo "Les résultats du seqkit de l'ensemble des variants sont aussi disponible dans le répertoire : $1/results_seqkit_all"
+  cat $repertoire_Ines/seqkit/results_seqkit_all.txt
+  echo "Les résultats du seqkit de l'ensemble des variants sont aussi disponible dans le répertoire : $repertoire_Ines/seqkit/results_seqkit_all"
   echo "Fin du programme"
   exit
 }
@@ -83,14 +84,14 @@ bool_seqkit='n'
 
 read -p "Réaliser une analyse avec seqkit pour déterminer la taille de fragment minimun ? (y/n)" bool_seqkit
 
-if [ "$bool_seqkit" == "y" ] || [ "$bool_seqkit" == "yes" ]; then 
+if [ "$bool_seqkit" == "y" ] || [ "$bool_seqkit" == "yes" ] || [ "$bool_seqkit" == "oui" ]; then 
 {
-   if [ ! -d $repertoire_seqkit ]; then #si le repertoire n'existe pas on le crée
+   if [ ! -d $repertoire_Ines/seqkit/ ]; then #si le repertoire n'existe pas on le crée
    {
-      mkdir $repertoire_seqkit
+      mkdir $repertoire_Ines/seqkit/
    }
    fi
-   seqkit_stats2 $repertoire_seqkit
+   seqkit_stats2
    
 } else {
 read -p "Voulez-vous effectuer le pipeline sur toutes les sequences ? (y/n) " read_entier
@@ -100,16 +101,16 @@ fi
 
 for element in $list_P
 do
-  if [ "$read_entier" == "n" ] || [ "$read_entier" == "no" ]; then #lancement un par un activer
+  if [ "$read_entier" == "n" ] || [ "$read_entier" == "no" ] || [ "$read_entier" == "non" ]; then #lancement un par un activer
   {
       read -p "Voulez-vous lancer le $element ? (y/n) " read_P
-      if [ "$read_P" == "y" ] || [ "$read_P" == "yes" ] ; then #lancement un par un
+      if [ "$read_P" == "y" ] || [ "$read_P" == "yes" ] || [ "$read_P" == "oui" ]; then #lancement un par un
       {
           run $element $number $number_lenght
       }
       fi
   }
-  else [ "$read_entier" == "y" ] || [ "$read_entier" == "yes" ] # lancement entier
+  else [ "$read_entier" == "y" ] || [ "$read_entier" == "yes" ] || [ "$read_entier" == "oui" ] # lancement entier
   {
       run $element $number $number_lenght
    }
